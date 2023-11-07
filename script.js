641dd7e9a38c32329cb54df5d04fe1c6c2f60d7d
farihaAfzaal13 <144538824+farihaAfzaal13@users.noreply.github.com>
2023-11-07 22:51:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,95 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const baseUrl = "https://jsonplaceholder.typicode.com/todos";
-    const todoList = document.getElementById("todo-list");
-    const addTodoButton = document.getElementById("add-todo");
-    const todoTitleInput = document.getElementById("todo-title");
+    const apiUrl = "https://jsonplaceholder.typicode.com/posts";
+    const postList = document.getElementById("post-list");
+    const createPostButton = document.getElementById("create-post");
+    const postTitleInput = document.getElementById("post-title");
+    const postBodyInput = document.getElementById("post-body");
 
-    async function fetchTodos() {
-        todoList.innerHTML = '';
+    function fetchPosts() {
+        postList.innerHTML = '';
 
-        try {
-            const response = await fetch(baseUrl);
-            const data = await response.json();
-
-            data.forEach(todo => {
-                const listItem = document.createElement("li");
-                listItem.textContent = todo.title;
-                todoList.appendChild(listItem);
-            });
-        } catch (error) {
-            console.error("Error fetching todos:", error);
-        }
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(posts => {
+                posts.forEach(post => {
+                    const listItem = document.createElement("li");
+                    listItem.innerHTML = `
+                        <span>${post.title}</span>
+                        <button class="edit-post" data-id="${post.id}">Edit</button>
+                        <button class="delete-post" data-id="${post.id}">Delete</button>
+                    `;
+                    postList.appendChild(listItem);
+                });
+            })
+            .catch(error => console.error("Error fetching posts:", error));
     }
-    
-    addTodoButton.addEventListener("click", async function () {
-        const title = todoTitleInput.value;
 
-        try {
-            const response = await fetch(baseUrl, {
-                method: 'POST',
-                body: JSON.stringify({ title, completed: false }),
-                headers: {
-                    'Content-type': 'application/json; charset=UTF-8',
-                },
-            });
+    createPostButton.addEventListener("click", function () {
+        const title = postTitleInput.value;
+        const body = postBodyInput.value;
 
+        fetch(apiUrl, {
+            method: 'POST',
+            body: JSON.stringify({ title, body }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        .then(response => {
             if (response.ok) {
-                await fetchTodos();
-                todoTitleInput.value = '';
+                postTitleInput.value = '';
+                postBodyInput.value = '';
+                fetchPosts();
             } else {
-                console.error("Error creating the todo.");
+                console.error("Error creating the post.");
             }
-        } catch (error) {
-            console.error("Error creating the todo:", error);
+        })
+        .catch(error => console.error("Error creating the post:", error));
+    });
+
+    postList.addEventListener("click", function (e) {
+        if (e.target.classList.contains("edit-post")) {
+            const postId = e.target.getAttribute("data-id");
+            const listItem = e.target.parentElement;
+            const postTitle = listItem.querySelector("span");
+            const newTitle = prompt("Edit post title:", postTitle.textContent);
+
+            if (newTitle !== null) {
+                fetch(`${apiUrl}/${postId}`, {
+                    method: 'PUT',
+                    body: JSON.stringify({ title: newTitle }),
+                    headers: {
+                        'Content-type': 'application/json; charset=UTF-8',
+                    },
+                })
+                .then(response => {
+                    if (response.ok) {
+                        postTitle.textContent = newTitle;
+                    } else {
+                        console.error("Error updating the post.");
+                    }
+                })
+                .catch(error => console.error("Error updating the post:", error));
+            }
+        }
+
+        if (e.target.classList.contains("delete-post")) {
+            const postId = e.target.getAttribute("data-id");
+            const listItem = e.target.parentElement;
+
+            fetch(`${apiUrl}/${postId}`, {
+                method: 'DELETE',
+            })
+            .then(response => {
+                if (response.ok) {
+                    listItem.remove();
+                } else {
+                    console.error("Error deleting the post.");
+                }
+            })
+            .catch(error => console.error("Error deleting the post:", error));
         }
     });
+
+    fetchPosts();
 });
